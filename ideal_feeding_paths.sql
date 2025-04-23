768889ab09a13e20725f1171a96906b557637445
Helana <helanan@gmail.com>
2025-04-23 12:24:30 -0400

    Create ideal_feeding_paths.sql

---
+++ ideal_feeding_paths.sql
@@ -0,0 +1,15 @@
+**Core Idea: Predicting and Supporting Feeding Schedules & Transitions**
+
+Here we’ll predict:
+
+- When the **next feed** is likely due
+- What **type** (tube, bottle, combo)
+- How much volume is typical
+- If baby might need **extra intervention** (missed feed, poor tolerance, etc.)
+
+This helps parents/caregivers:
+- Stick to medically-safe feeding schedules
+- Recognize when a baby is trending toward oral/bottle feeds
+- Catch patterns (e.g. frequent skipped feeds, low tolerance)
+- Be proactive with transitions or alerts
+
